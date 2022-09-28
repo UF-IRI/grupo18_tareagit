ac28806a05937220766513e95492785d46af4c26
FabricioMacri <fabriciomacri@hotmail.com>
2022-09-28 00:12:36 -0300

    Mas casos de testeo y comentarios

--- libreria/funcion1.cpp
+++ libreria/funcion1.cpp
@@ -8,16 +8,16 @@ float calcular_promedio(int puntero[100], int N)
     int* auxiliar;
     auxiliar = new int[N]; // asigno memoria para un vector de N elementos y guardo la direccion base en puntero
     for (int i = 0; i < N; i++)
-    {
+    {                                       //Recorremos el array y acumulamos los datos en memoria dinamica
         *auxiliar = puntero[i];
         acumulador = acumulador + *auxiliar;
     }
-    if (N != 0)
+    if (N != 0)                           // Controlamos que el acumulador sea mayor que 0 para que no divida por 0
     {
         promedio = acumulador / N;
         return promedio;
     }
-    else {
+    else {                               //Si el denominador es 0 retornamos -1 para avisar que hay un error
         return -1;
     }
 }
@@ -30,13 +30,14 @@ int numerosPares(int puntero[100], int N)
     auxiliar = new int[N];
     for (int i = 0; i < N; i++)
     {
-        *auxiliar = puntero[i];
+        *auxiliar = puntero[i];                         //Recorremos el array y en memoria dinamica revisamos los restos para ver si 
+                                                       //es multiplo de 2
         if (*auxiliar % 2 == 0) {
             contador++;
         }
     }
 
-    if (contador != 0)return contador;
+    if (contador != 0)return contador;               //Si hay multiplos de 2 los devolvemos y para decir q no los hubo retornamos -1
 
     else return -1;
 
--- unit-test/funcion1.cpp
+++ unit-test/funcion1.cpp
@@ -10,13 +10,27 @@ TEST(funciones1_funciones_Test, funcione1) {
 
 	int array1[100] = { 2, 4, 6 };
 
-	ASSERT_THAT(calcular_promedio(array1, 3), 4);
+	ASSERT_THAT(calcular_promedio(array1, 3), 4);     //Funciona todo bien
 	}
+
+TEST(funciones1_funciones_Test, funcione11) {
+
+	int array1[100] = { 2, 4, 6};
+
+	ASSERT_THAT(calcular_promedio(array1, 0), -1);   //Es correcto y devuelve -1 que es lo esperado para los errores
+}
 
 	TEST(funciones1_funciones_Test, funcione2) {
 
 		int array2[100] = { 2, 5, 6 };
 
-		ASSERT_THAT(numerosPares(array2, 3), 2);
+		ASSERT_THAT(numerosPares(array2, 3), 2);    //Funciona todo bien
+	}
+
+	TEST(funciones1_funciones_Test, funcione22) {
+
+		int array1[100] = { 3, 5, 11 };
+
+		ASSERT_THAT(numerosPares(array1, 3), -1);   //Es correcto y devuelve -1 que es lo esperado para los errores
 	}
 
